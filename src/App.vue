27fd9a0d906a84d1d67bd5b6f0535159ae329cbb
Sergey Kravcov <sergey.kravcov.job@gmail.com>
2022-09-04 18:35:24 +0400

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
